c11c83c530adfb093eb69e4c15f06c9ec78b40b5
jsquared71 <jsquared71@gmail.com>
2016-04-13 17:49:12 -0400

    Disabled text

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.listen(app.get('port'));
 
 app.use(bodyParser.json());
 
+/**
 app.get('/', function(req, res) {
   console.log(req);
   res.send('It works!');
 });
+*/
 
-/**
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
     res.send(req.query['hub.challenge']);
@@ -40,5 +41,5 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200);
 });
-*/
+
 app.listen();
