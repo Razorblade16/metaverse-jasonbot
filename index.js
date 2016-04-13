6a9d1795ac9abfe6730e4b2bb8fa237e1f40d56c
jsquared71 <jsquared71@gmail.com>
2016-04-13 18:16:33 -0400

    Correcting files

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('It works!');
+});
+
 app.get('/', function (req, res) {
   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
     res.send(req.query['hub.challenge']);
@@ -17,3 +39,6 @@ app.post('/', function (req, res) {
   }
   res.sendStatus(200);
 });
+
+
+app.listen();
