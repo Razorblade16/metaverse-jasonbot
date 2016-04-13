ca4dfebe0d0513c08d9fd62867c0cf3ea9c2f272
jsquared71 <jsquared71@gmail.com>
2016-04-13 17:57:46 -0400

    Removed everything except bot apit

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
-
-app.use(bodyParser.json());
-
-/**
-app.get('/', function(req, res) {
-  console.log(req);
-  res.send('It works!');
-});
-*/
-
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
     res.send(req.query['hub.challenge']);
@@ -41,5 +17,3 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200);
 });
-
-app.listen();
