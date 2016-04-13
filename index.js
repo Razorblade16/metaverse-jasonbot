6d750f71e9551e0f39e7efb470da0b49692879ae
jsquared71 <jsquared71@gmail.com>
2016-04-13 18:02:48 -0400

    URL correction

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-app.get('/webhook/', function (req, res) {
+app.get('/', function (req, res) {
   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 })
 
-app.post('/webhook/', function (req, res) {
+app.post('/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
