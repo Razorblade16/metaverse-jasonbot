5674262e7475d8b087e11694e520713977b71a00
jsquared71 <jsquared71@gmail.com>
2016-04-13 16:39:11 -0400

    Added files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('It works!');
+});
+
+/**
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+*/
+app.listen();
